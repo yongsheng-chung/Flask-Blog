489a99b02491fa2310a548fc4c34b3a6b872575d
Yong Sheng Chung <johnson960521@gmail.com>
2020-09-25 09:28:01 +0800

    initial commit

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Yong Sheng',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'September 5, 2020'
+    },
+    {
+        'author': 'Pancake Chuah',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 5, 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
